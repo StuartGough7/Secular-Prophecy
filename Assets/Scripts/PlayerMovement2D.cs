74835ce69b668ef321d59c1018fed57ec5e90db8
Stuart Gough <stuartgough7@gmail.com>
2020-01-05 00:02:55 +0000

    Character controller trimmed with no groundcheck

--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -7,13 +7,19 @@ public class PlayerMovement2D : MonoBehaviour
     public CharacterController2D controller;
     float horizontalMove = 0f;
     public float movementSpeed = 40f;
+    bool isJumping = false;
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;   
+        horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
     }
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, isJumping);
+        isJumping = false;
     }
 }
