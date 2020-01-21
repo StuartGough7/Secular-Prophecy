fbec7e065968c0bf6b116e293fd9921d742ac986
Stuart Gough <stuartgough7@gmail.com>
2020-01-21 22:21:39 +0000

    Basic Cinemachine cursor follow

---
+++ Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 10.0f; // This was very important as the camera is 10f away we need to specify what relative position from the camera we in z we want to know about
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = mouseWorldPosition;
+    }
+}
