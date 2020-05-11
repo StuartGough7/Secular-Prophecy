23c2bbfcddcf6d79df86741e01cb81a05f1dca66
Stuart Gough <stuartgough7@gmail.com>
2020-05-11 21:50:03 +0100

    Playroom Implemented with Character Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+// This script moves the character controller forward
+// and sideways based on the arrow keys.
+// It also jumps when pressing space.
+// Make sure to attach a character controller to the same game object.
+// It is recommended that you make only one call to Move or SimpleMove per frame.
+
+public class PlayerController : MonoBehaviour
+{
+  CharacterController characterController;
+
+  public float speed = 6.0f;
+  public float jumpSpeed = 8.0f;
+  public float gravity = 20.0f;
+
+  float x = 0f;
+  float y = 0f;
+
+  private Vector3 moveDirection = Vector3.zero;
+
+  void Start()
+  {
+    characterController = GetComponent<CharacterController>();
+  }
+
+  void Update()
+  {
+    if (characterController.isGrounded)
+    {
+      // We are grounded, so recalculate
+      // move direction directly from axes
+      x = Input.GetAxis("Horizontal");
+      y = Input.GetAxis("Vertical");
+
+      moveDirection = transform.right * x + transform.forward * y;
+
+      if (Input.GetButton("Jump"))
+      {
+        moveDirection.y = jumpSpeed;
+      }
+    }
+
+    // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+    // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+    // as an acceleration (ms^-2)
+    moveDirection.y -= gravity * Time.deltaTime;
+
+    // Move the controller
+    characterController.Move(moveDirection * speed * Time.deltaTime);
+  }
+}
