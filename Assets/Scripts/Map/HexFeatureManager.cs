092cc641c8a42f24f0f714447b3759abbad1a525
Stuart Gough <stuartgough7@gmail.com>
2020-05-27 23:43:49 +0100

    Added wall towers and ensure no floating

--- Assets/Scripts/Map/HexFeatureManager.cs
+++ Assets/Scripts/Map/HexFeatureManager.cs
@@ -6,6 +6,8 @@ public class HexFeatureManager : MonoBehaviour {
 
   public HexMesh walls;
 
+  public Transform wallTower;
+
   Transform container;
 
   public void Clear() {
@@ -115,7 +117,8 @@ public class HexFeatureManager : MonoBehaviour {
   }
 
   void AddWallSegment(
-    Vector3 nearLeft, Vector3 farLeft, Vector3 nearRight, Vector3 farRight
+    Vector3 nearLeft, Vector3 farLeft, Vector3 nearRight, Vector3 farRight,
+    bool addTower = false
   ) {
     nearLeft = HexMetrics.Perturb(nearLeft);
     farLeft = HexMetrics.Perturb(farLeft);
@@ -144,6 +147,14 @@ public class HexFeatureManager : MonoBehaviour {
     v4.y = rightTop;
     walls.AddQuadUnperturbed(v2, v1, v4, v3);
     walls.AddQuadUnperturbed(t1, t2, v3, v4);
+    if (addTower) {
+      Transform towerInstance = Instantiate(wallTower);
+      towerInstance.transform.localPosition = (left + right) * 0.5f;
+      Vector3 rightDirection = right - left;
+      rightDirection.y = 0f;
+      towerInstance.transform.right = rightDirection;
+      towerInstance.SetParent(container, false);
+    }
   }
 
   void AddWallSegment(
@@ -159,7 +170,14 @@ public class HexFeatureManager : MonoBehaviour {
 
     if (hasLeftWall) {
       if (hasRighWall) {
-        AddWallSegment(pivot, left, pivot, right);
+        bool hasTower = false;
+        if (leftCell.Elevation == rightCell.Elevation) {
+          HexHash hash = HexMetrics.SampleHashGrid(
+            (pivot + left + right) * (1f / 3f)
+          );
+          hasTower = hash.e < HexMetrics.wallTowerThreshold;
+        }
+        AddWallSegment(pivot, left, pivot, right, hasTower);
       } else if (leftCell.Elevation < rightCell.Elevation) {
         AddWallWedge(pivot, left, right);
       } else {
--- Assets/Scripts/Map/HexMetrics.cs
+++ Assets/Scripts/Map/HexMetrics.cs
@@ -45,11 +45,15 @@ public static class HexMetrics {
 
   static HexHash[] hashGrid;
 
-  public const float wallHeight = 3f;
+  public const float wallHeight = 4f;
+
+  public const float wallYOffset = -1f;
 
   public const float wallThickness = 0.75f;
 
   public const float wallElevationOffset = verticalTerraceStepSize;
+
+  public const float wallTowerThreshold = 0.5f;
 
   static Vector3[] corners = {
     new Vector3(0f, 0f, outerRadius),
@@ -187,7 +191,7 @@ public static class HexMetrics {
     near.z += (far.z - near.z) * 0.5f;
     float v =
       near.y < far.y ? wallElevationOffset : (1f - wallElevationOffset);
-    near.y += (far.y - near.y) * v;
+    near.y += (far.y - near.y) * v + wallYOffset;
     return near;
   }
 }
